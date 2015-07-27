b667637d084b184b1f80b605c451ea55d6fb8c86
weizhi9958 <as54376520@gmail.com>
2015-07-27 23:14:53 +0800

    7/27 Range AlertDialog

--- app/src/main/java/com/softmobile/bingo/base/MainActivity.java
+++ app/src/main/java/com/softmobile/bingo/base/MainActivity.java
@@ -1,18 +1,76 @@
 package com.softmobile.bingo.base;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    TextView tvNowRange;
+
+    String strRangeMin = null;
+    String strRangeMax = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNowRange = (TextView) findViewById(R.id.tvNowRange);
+
+        onCreateDialog();
+
+    }
+
+
+    public void onCreateDialog() {
+        final AlertDialog.Builder adbBuilder = new AlertDialog.Builder(MainActivity.this);
+        final LayoutInflater inflater = this.getLayoutInflater();
+        final View viewDialog = inflater.inflate(R.layout.dialog_range, null);
+
+        adbBuilder.setView(viewDialog); //設置view來源為R.layout.dialog_range
+        adbBuilder.setCancelable(false);
+        adbBuilder.setPositiveButton(R.string.enter, null);
+
+        final AlertDialog alert = adbBuilder.create(); //取代原本的adbBuilder
+        alert.show();
+        Button btnDialogEnter = alert.getButton(AlertDialog.BUTTON_POSITIVE);  //取得alert的button
+        btnDialogEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //取得alertdialog的兩個edittext
+                EditText etRangeMin = (EditText) viewDialog.findViewById(R.id.etRangeMin);
+                EditText etRangeMax = (EditText) viewDialog.findViewById(R.id.etRangeMax);
+
+                //如果皆有輸入值存入string變數 , 否則toast
+                if (!"".equals(etRangeMin.getText().toString()) && !"".equals(etRangeMax.getText().toString())) {
+
+                    //判斷最大值最小值 , 由小到大
+                    if (Integer.parseInt(etRangeMax.getText().toString()) > Integer.parseInt(etRangeMin.getText().toString())) {
+                        strRangeMin = etRangeMin.getText().toString();
+                        strRangeMax = etRangeMax.getText().toString();
+                    } else {
+                        strRangeMin = etRangeMax.getText().toString();
+                        strRangeMax = etRangeMin.getText().toString();
+                    }
+
+                    //將範圍顯示至tvNoewRange , 並關閉alert
+                    tvNowRange.setText(strRangeMin + " ~ " + strRangeMax);
+                    alert.dismiss();
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.dialogToast_Null, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -36,4 +94,6 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
